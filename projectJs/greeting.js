5d56ee70645c002fe555564725485e0da22b1dbf
mikekang47 <gyeong0308@gmail.com>
2021-07-29 22:44:44 +0900

    Resume the project Start to make clock

---
+++ projectJs/greeting.js
@@ -0,0 +1,27 @@
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(inputname) {
+    greeting.innerText = `Hello ${inputname}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings();
+}   
+  
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+  if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME); 
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}    
